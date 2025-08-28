53c75ba0924f2843ff735890c9a85a5d952fab72
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-08-28 11:19:55 -0300

    chore: agregar componente WindowFrame y definir tipos en vue-libvasak

---
+++ src/layouts/WindowLayout.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import { WindowFrame } from "@vasakgroup/vue-libvasak";
+</script>
+<template>
+  <div style="background-color: aqua; height: 100vh; width: 100vw">
+    <WindowFrame>
+      <slot />
+    </WindowFrame>
+  </div>
+</template>
---
+++ src/types/vue-libvasak.d.ts
@@ -0,0 +1,5 @@
+import { Component } from "vue";
+
+declare module "@vasakgroup/vue-libvasak" {
+  export const WindowFrame: Component;
+}
