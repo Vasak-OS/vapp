b0aed53f7b3f7d160f29ecc533f7e364857fb670
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2025-08-28 11:19:42 -0300

    chore: agregar permisos de vicons y config-manager en default.json y registrar plugins en lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,6 +5,8 @@ mod icons;
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_config_manager::init())
+        .plugin(tauri_plugin_vicons::init())
         .invoke_handler(tauri::generate_handler![
             icons::get_icon_base64,
             icons::get_symbol_base64
