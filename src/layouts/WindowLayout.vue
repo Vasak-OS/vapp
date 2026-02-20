7f3672d6e4a7cc99d6267f92733b3d2461838dbe
joaquin (Pato) Decima <jdecima@vasak.net.ar>
2026-02-20 18:29:50 -0300

    feat: mejorar estilos de ventana y actualizar variables de color para un mejor tema

--- src/layouts/WindowLayout.vue
+++ src/layouts/WindowLayout.vue
@@ -3,8 +3,8 @@
 import { WindowFrame } from '@vasakgroup/vue-libvasak';
 </script>
 <template>
-    <WindowFrame class="flex rounded-t-window bg-vsk-primary justify-between items-center h-6 h-8 inline-block px-4 py-1">
-      <div class="h-[calc(100vh-32px)] rounded-b-window background">
+    <WindowFrame class="flex rounded-t-corner-window bg-primary dark:bg-primary-dark justify-between items-center h-6 h-8 inline-block px-4 py-1">
+      <div class="h-[calc(100vh-32px)] rounded-b-corner-window background">
         <slot />
       </div>
     </WindowFrame>
