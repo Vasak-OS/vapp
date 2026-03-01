09ff274185212bc277eaa4699df4664b8de814e8
joaquin (Pato) Decima <jdecima@vasak.net.ar>
2026-03-01 13:36:45 -0300

    feat: agregar archivo CSS principal y ajustar importaciones en main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
-import App from './App.vue';
-import './style.css';
+import App from '@/App.vue';
+import '@/assets/main.css';
 
 const app = createApp(App);
 const pinia = createPinia();
